72e81209a2a9df4bf362622c0b751980f80536c8
liguanghao <chiansoft_lgh@outlook.com>
2017-05-12 21:54:18 +0800

    daily

--- app/daily/busieness/components/dashboard.component.ts
+++ app/daily/busieness/components/dashboard.component.ts
@@ -10,130 +10,144 @@ import {RemoteService} from "../../service/RemoteService";
     styleUrls: ['../styles/dashboard.css'],
     providers: [RemoteService]
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
     ngOnInit(): void {
-        this.remote.post("http://localhost:8080/daily/test/test2",{name:"liguanghao"},{},data =>console.log(data))
-
+        this.remote.post("http://localhost:8080/daily/dashboard/trendStatistics", {name: "liguanghao"}, {}, data => this.init(data))
     }
 
-    constructor(private remote:RemoteService){}
-    option = {
-    title : {
-        text: '南丁格尔玫瑰图',
-        subtext: '纯属虚构',
-        x:'center'
-    },
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        x : 'center',
-        y : 'bottom',
-        data:['rose1','rose2','rose3','rose4','rose5','rose6','rose7','rose8']
-    },
-    toolbox: {
-        show : true,
-        feature : {
-            mark : {show: true},
-            dataView : {show: true, readOnly: false},
-            magicType : {
-                show: true,
-                type: ['pie', 'funnel']
-            },
-            restore : {show: true},
-            saveAsImage : {show: true}
-        }
-    },
-    calculable : true,
-    series : [
-        {
-            name:'半径模式',
-            type:'pie',
-            radius : [20, 110],
-            center : ['25%', '50%'],
-            roseType : 'radius',
-            label: {
-                normal: {
-                    show: false
-                },
-                emphasis: {
-                    show: true
-                }
-            },
-            lableLine: {
-                normal: {
-                    show: false
-                },
-                emphasis: {
-                    show: true
-                }
-            },
-            data:[
-                {value:10, name:'rose1'},
-                {value:5, name:'rose2'},
-                {value:15, name:'rose3'},
-                {value:25, name:'rose4'},
-                {value:20, name:'rose5'},
-                {value:35, name:'rose6'},
-                {value:30, name:'rose7'},
-                {value:40, name:'rose8'}
-            ]
-        },
-        {
-            name:'面积模式',
-            type:'pie',
-            radius : [30, 110],
-            center : ['75%', '50%'],
-            roseType : 'area',
-            data:[
-                {value:10, name:'rose1'},
-                {value:5, name:'rose2'},
-                {value:15, name:'rose3'},
-                {value:25, name:'rose4'},
-                {value:20, name:'rose5'},
-                {value:35, name:'rose6'},
-                {value:30, name:'rose7'},
-                {value:40, name:'rose8'}
-            ]
-        }
-    ]
-};
+    constructor(private remote: RemoteService) {
+    }
+
+//     option = {
+//     title : {
+//         text: '南丁格尔玫瑰图',
+//         subtext: '纯属虚构',
+//         x:'center'
+//     },
+//     tooltip : {
+//         trigger: 'item',
+//         formatter: "{a} <br/>{b} : {c} ({d}%)"
+//     },
+//     legend: {
+//         x : 'center',
+//         y : 'bottom',
+//         data:['rose1','rose2','rose3','rose4','rose5','rose6','rose7','rose8']
+//     },
+//     toolbox: {
+//         show : true,
+//         feature : {
+//             mark : {show: true},
+//             dataView : {show: true, readOnly: false},
+//             magicType : {
+//                 show: true,
+//                 type: ['pie', 'funnel']
+//             },
+//             restore : {show: true},
+//             saveAsImage : {show: true}
+//         }
+//     },
+//     calculable : true,
+//     series : [
+//         {
+//             name:'半径模式',
+//             type:'pie',
+//             radius : [20, 110],
+//             center : ['25%', '50%'],
+//             roseType : 'radius',
+//             label: {
+//                 normal: {
+//                     show: false
+//                 },
+//                 emphasis: {
+//                     show: true
+//                 }
+//             },
+//             lableLine: {
+//                 normal: {
+//                     show: false
+//                 },
+//                 emphasis: {
+//                     show: true
+//                 }
+//             },
+//             data:[
+//                 {value:10, name:'rose1'},
+//                 {value:5, name:'rose2'},
+//                 {value:15, name:'rose3'},
+//                 {value:25, name:'rose4'},
+//                 {value:20, name:'rose5'},
+//                 {value:35, name:'rose6'},
+//                 {value:30, name:'rose7'},
+//                 {value:40, name:'rose8'}
+//             ]
+//         },
+//         {
+//             name:'面积模式',
+//             type:'pie',
+//             radius : [30, 110],
+//             center : ['75%', '50%'],
+//             roseType : 'area',
+//             data:[
+//                 {value:10, name:'rose1'},
+//                 {value:5, name:'rose2'},
+//                 {value:15, name:'rose3'},
+//                 {value:25, name:'rose4'},
+//                 {value:20, name:'rose5'},
+//                 {value:35, name:'rose6'},
+//                 {value:30, name:'rose7'},
+//                 {value:40, name:'rose8'}
+//             ]
+//         }
+//     ]
+// };
 
     option1 = {
-    title: {
-        text: '基础雷达图'
-    },
-    tooltip: {},
-    legend: {
-        data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
-    },
-    radar: {
-        // shape: 'circle',
-        indicator: [
-            { name: '销售（sales）', max: 6500},
-            { name: '管理（Administration）', max: 16000},
-            { name: '信息技术（Information Techology）', max: 30000},
-            { name: '客服（Customer Support）', max: 38000},
-            { name: '研发（Development）', max: 52000},
-            { name: '市场（Marketing）', max: 25000}
-        ]
-    },
-    series: [{
-        name: '预算 vs 开销（Budget vs spending）',
-        type: 'radar',
-        // areaStyle: {normal: {}},
-        data : [
-            {
-                value : [4300, 10000, 28000, 35000, 50000, 19000],
-                name : '预算分配（Allocated Budget）'
-            },
-            {
-                value : [5000, 14000, 28000, 31000, 42000, 21000],
-                name : '实际开销（Actual Spending）'
-            }
-        ]
-    }]
-};
+        title: {
+            text: '我的日常偏向'
+        },
+        tooltip: {},
+        legend: {
+            data: ['日常偏向']
+        },
+        radar: {
+            // shape: 'circle'
+            indicator: [
+                {name: '学习', max: 10},
+                {name: '家庭', max: 10},
+                {name: '工作', max: 10},
+                {name: '休闲', max: 10},
+                {name: '思考', max: 10},
+                {name: '娱乐', max: 10}]
+        },
+        series: [{
+            name: '日常偏向',
+            type: 'radar',
+            // areaStyle: {normal: {}},
+            data: [
+                {value : [0, 0, 0, 0, 0, 0],name : '日常偏向'}
+                ]
+        }]
+    };
 
+    private init(data: any) {
+        this.option1 = {
+            title: {
+                text: '我的日常偏向'
+            },
+            tooltip: {},
+            legend: {
+                data: ['日常偏向']
+            },
+            radar: {
+                // shape: 'circle'
+                indicator: data.indicator
+            },
+            series: [{
+                name: '日常偏向',
+                type: 'radar',
+                // areaStyle: {normal: {}},
+                data: data.data
+            }]
+        };
+    }
 }
